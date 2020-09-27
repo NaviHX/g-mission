4442123e62434f9f7fc16730d2f80c37ac3669c9
NaviHX <932942142@qq.com>
2020-09-27 11:16:28 +0800

    统一代码风格

--- include/g-mision.h
+++ include/g-mision.h
@@ -1,22 +1,22 @@
 #ifndef __G_MISSION_H__
 #define __G_MISSION_H__
 
-typedef struct NODE//链表节点
+typedef struct NODE //链表节点
 {
-    int data;//编号
-    struct NODE* next;//后继结点指针
-}node,*nodeptr;
+    int data;          //编号
+    struct NODE *next; //后继结点指针
+} node, *nodeptr;
 
-typedef struct LINK//链表
+typedef struct LINK //链表
 {
-    nodeptr head;//链表头节点
-    int size;//链表大小
-}link;
+    nodeptr head; //链表头节点
+    int size;     //链表大小
+} link;
 
 void function1Solve(int n);
-void function2Vertify(int n,int x,int y);
-int judge(int n,int x,int y);
+void function2Vertify(int n, int x, int y);
+int judge(int n, int x, int y);
 link createLink(int n);
-void deleteNode(nodeptr prev,nodeptr now);
+void deleteNode(nodeptr prev, nodeptr now);
 
 #endif
