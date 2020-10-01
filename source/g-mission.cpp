d5e0f23cf57074781b1a57ceddeec8f69a4cc0e4
NaviHX <932942142@qq.com>
2020-10-01 09:45:42 +0800

    添加注释

--- source/g-mission.cpp
+++ source/g-mission.cpp
@@ -9,6 +9,7 @@ void function1Solve(int n)
     else
     {
         int i,j;
+        //构造二重循环遍历所有可能的x,y值寻找解
         for(i=1;i<=n;i++)
             for(j=1;j<=n;j++)
                 if(judge(n,i,j))
@@ -33,12 +34,14 @@ link createLink(int n)
 {
     link ret;
     nodeptr temp = NULL;
+    //初始化链表数据,添加空头节点
     ret.size = n;
     ret.head = (nodeptr)malloc(sizeof(node));
     ret.head->data = 0;
     ret.head->next = ret.head;
     while(n)
     {
+        //将新节点添加到链表中
         temp = (nodeptr)malloc(sizeof(node));
         temp->next = ret.head->next;
         temp->data = n;
@@ -59,6 +62,7 @@ int judge(int n,int x,int y)
     link list = createLink(n);
     int cnt;
     nodeptr cur=list.head->next, pre=list.head;
+    //将cur移动到x,pre移动到x前驱
     while(cur->data!=x)
     {
         cur = cur->next;
@@ -67,6 +71,7 @@ int judge(int n,int x,int y)
     while(list.size>1)
     {
         cnt = 0;
+        //0号头节点不参与报数,循环不结束
         while(cnt<y-1 || cur->data==0)
         {
             if(cur->data!=0)
@@ -74,10 +79,12 @@ int judge(int n,int x,int y)
             cur = cur->next;
             pre = pre->next;
         }
+        //防止链表丢失,先将cur移动到后一位
         nodeptr temp = cur;
         cur = cur->next;
         deleteNode(pre, temp);
         list.size--;
     }
+    //判断剩下的最后一个节点是否为1号加里森
     return list.head->next->data == 1;
 }
